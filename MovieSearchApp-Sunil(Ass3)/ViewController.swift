a7591724b3c94fcfa4a989b5e5d12d82a02496d4
sunil3696 <sunilstark3696@gmail.com>
2024-07-25 04:39:49 -0400

    Initial Commit

---
+++ MovieSearchApp-Sunil(Ass3)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchApp-Sunil(Ass3)
+//
+//  Created by Sunil Balami on 2024-07-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieSearchApp-Sunil(Ass3)UITests/MovieSearchApp_Sunil_Ass3_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieSearchApp_Sunil_Ass3_UITestsLaunchTests.swift
+//  MovieSearchApp-Sunil(Ass3)UITests
+//
+//  Created by Sunil Balami on 2024-07-25.
+//
+
+import XCTest
+
+final class MovieSearchApp_Sunil_Ass3_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
